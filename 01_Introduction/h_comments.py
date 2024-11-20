66690a57b268ff0d3b916e43e3c84c5043ddaa3c
Manideepreddy-commits <Mthodeti_gps@nec.edu>
2024-11-20 16:12:58 +0000

    Class03-Introduction and Basic Arithmatic operations

---
+++ 01_Introduction/h_comments.py
@@ -0,0 +1,31 @@
+#!user/bin/python3
+"""
+PURPOSE:   working with comment opertor
+        - Once # is encounterd, complete line from the position is ignored.
+        -PEP 8 recommends one white space after # operator.
+        -comments 
+              -single line comment #
+              -multiline comment python does not supports
+
+Question: Why python dont have multiline comment operator
+
+Answer: Python is a interpreter based language means each line executes.
+
+Python code-> line by line ->c code ->assembly ->machine code
+
+print("Hello world")
+
+"""
+print("Hello world2")
+#print("Hello world3")
+
+# any operator within quotes with be treated as ordinory character.
+print("hello #world4")
+print("Hello","world5",sep="#")
+
+print("Hello world6")#123kj123123
+print("Hello world7")#this is about the world
+print("hello world7")#
+#print("hello world8"#)
+
+#CTRL +/  ->Multi_line_comment/uncomment
